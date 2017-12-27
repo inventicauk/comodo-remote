875e2ab77bc4a73f339a195f7ac4044b46d7b5c6
inventicauk <Jeremy@inventica.co.uk>
2017-12-27 14:50:32 +0000

    Chocolatey Package for COMODO Remote Control
    Comodo Remote Control for COMODO One ITSM / RMM http://one.comodo.com/

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.cmdm.comodo.com/download/ComodoRemoteControl.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'COMODO Remote Control*'
+  checksum      = '0BFBCCF44C609CE85B3B1B77115503101FBAB8152FFE5A3789F73FB3047B8522'
+  checksumType  = 'sha256'
+  silentArgs    = "/silent"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
